64145c43f7d8f8ecd57ede8c91787803953f96ee
James Milgram <214770459+jamesmml@users.noreply.github.com>
2026-08-27 02:10:45 -0500

    Fixed order book updating logic

--- l2-order-book-exploration/exploration_utils.py
+++ l2-order-book-exploration/exploration_utils.py
@@ -15,6 +15,82 @@ import json
 import pandas as pd
 from decimal import Decimal
 
+def update_order_book(order_book_update: list, local_order_book: list) -> None:
+    """
+    Updates a local order book in place to reflect new order information.
+
+    Args:
+        order_book_update : An order book update
+        local_order_book : The current order book
+    """
+    # Loop through all updates and amend orders in the local book as needed
+    for update in order_book_update:
+        order_found = False
+        side = update["side"]
+        price_level = Decimal(update["price_level"])
+        new_quantity = update["new_quantity"]
+
+        while (not order_found):
+            order_book = local_order_book
+
+            # Logic for inserting new orders differs between bids and asks
+            if side == "bid":
+                for idx, order in enumerate(order_book):
+                    order_price = Decimal(order["price_level"])
+                    if order["side"] != "bid":
+                        continue
+                    elif order_price == price_level:
+                        order_found = True
+                        if new_quantity == "0":
+                            local_order_book.pop(idx)
+                        else:
+                            local_order_book[idx] = update
+                        break
+                    # If all bids are higher and the next order is an ask, then the new order is lowest bid
+                    elif order_price > price_level and order_book[idx + 1]["side"] == "offer":
+                        order_found = True
+                        local_order_book.insert(idx + 1, update)
+                        break
+                    # Skip higher bids otherwise
+                    elif order_price > price_level:
+                        continue
+                    # Using elif to be explicit: add a new order to the local book if a lower bid is found
+                    elif order_price < price_level:
+                        order_found = True
+                        local_order_book.insert(idx, update)
+                        break
+                    else:
+                        raise RuntimeError(f"Unexpected behavior: current order - {order}\n" + \
+                                f"current update - {update}")
+            else:
+                for idx, order in enumerate(order_book):
+                    order_price = Decimal(order["price_level"])
+                    if order["side"] != "offer":
+                        continue
+                    elif order_price == price_level:
+                        order_found = True
+                        if new_quantity == "0":
+                            local_order_book.pop(idx)
+                        else:
+                            local_order_book[idx] = update
+                        break
+                    # If all asks are lower, then the new order is the largest ask
+                    elif order_price < price_level and idx == len(order_book) - 1:
+                        order_found = True
+                        local_order_book.append(update)
+                        break
+                    # Skip lower asks otherwise
+                    elif order_price < price_level:
+                        continue
+                    # Using elif to be explicit: add a new order to the local book if a larger ask is found
+                    elif order_price > price_level:
+                        order_found = True
+                        local_order_book.insert(idx, update)
+                        break
+                    else:
+                        raise RuntimeError(f"Unexpected behavior: current order - {order}\n" + \
+                                f"current update - {update}")
+
 def compute_stats(currency: str, order_book_timestamp: str, order_book: list) -> dict:
     """
     Computes various order book statistics including the spread, mid price, and order book imbalance.
@@ -28,9 +104,10 @@ def compute_stats(currency: str, order_book_timestamp: str, order_book: list) ->
         stats : A dict containing the computed statistics
     """
     # Obtaining the top 5 bids and top 5 asks
-    # Coinbase response already sorts bids from highest to lowest and asks from lowest to highest
+    # The order book already sorts bids from highest to lowest and asks from lowest to highest
     top_bids = []
     top_asks = []
+
     for order in order_book:
         # Bids: what prices are people offering to buy a cryptocurrency in USD?
         if order["side"] == "bid" and len(top_bids) < 5:
@@ -68,66 +145,6 @@ def compute_stats(currency: str, order_book_timestamp: str, order_book: list) ->
 
     return stats
 
-def update_order_book(order_book_update: list, local_order_book: list) -> None:
-    """
-    Updates a local order book in place to reflect new order information.
-
-    Args:
-        order_book_update : An order book update
-        local_order_book : The current order book
-    """
-    # Loop through all updates and amend orders in the local book as needed
-    for update in order_book_update:
-        order_found = False
-        side = update["side"]
-        price_level = Decimal(update["price_level"])
-        new_quantity = update["new_quantity"]
-
-        while (not order_found):
-            order_book = local_order_book
-
-            # Logic for inserting new orders differs between bids and asks
-            if side == "bid":
-                for idx, order in enumerate(order_book):
-                    order_price = Decimal(order["price_level"])
-                    if order["side"] != "bid" or order_price > price_level:
-                        continue
-                    elif order_price == price_level:
-                        order_found = True
-                        if new_quantity == "0":
-                            local_order_book.pop(idx)
-                        else:
-                            local_order_book[idx] = update
-
-                        break
-                    # Using elif to be explicit: add a new order to the local book
-                    elif order_price < price_level:
-                        order_found = True
-                        local_order_book.insert(idx, update)
-                    else:
-                        raise ValueError(f"Unexpected behavior: current order - {order}\n" + \
-                                f"current update - {update}")
-            else:
-                for idx, order in enumerate(order_book):
-                    order_price = Decimal(order["price_level"])
-                    if order["side"] != "offer" or order_price < price_level:
-                        continue
-                    elif order_price == price_level:
-                        order_found = True
-                        if new_quantity == "0":
-                            local_order_book.pop(idx)
-                        else:
-                            local_order_book[idx] = update
-
-                        break
-                    # Using elif to be explicit: add a new order to the local book
-                    elif order_price > price_level:
-                        order_found = True
-                        local_order_book.insert(idx, update)
-                    else:
-                        raise ValueError(f"Unexpected behavior: current order - {order}\n" + \
-                                f"current update - {update}")
-
 async def parse_order_books(url: str, message: dict, currencies: list) -> None:
     """
     Function for parsing L2 order book data of various cryptocurrencies.
@@ -139,8 +156,8 @@ async def parse_order_books(url: str, message: dict, currencies: list) -> None:
     """
     DESIRED_CHANNEL = "l2_data"
 
-    # Creation of local order books
-    local_order_books = {currency:[] for currency in currencies}
+    # Accumulator of local order books
+    local_order_books = {currency:None for currency in currencies}
 
     # DataFrame for tracking order book evolution
     stats_df = pd.DataFrame()
@@ -155,12 +172,13 @@ async def parse_order_books(url: str, message: dict, currencies: list) -> None:
         # Loop for processing order book data
         while True:
             response = json.loads(await ws.recv())
-            response_contents = response["events"][0]
-            response_type = response_contents["type"]
 
             # Ignore responses that don't contain order book data
             if response["channel"] != DESIRED_CHANNEL:
                 continue
+
+            response_contents = response["events"][0]
+            response_type = response_contents["type"]
 
             # Access order book according to JSON structure
             currency = response_contents["product_id"]
